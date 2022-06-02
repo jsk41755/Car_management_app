cf786970a39755af95c27b7c045958c33df261e5
jsk41755 <jsk41755@naver.com>
2022-06-02 22:08:58 +0900

    알림기능 작성중

--- app/src/main/java/com/example/car_management_app/Settings.java
+++ app/src/main/java/com/example/car_management_app/Settings.java
@@ -1,10 +1,18 @@
 package com.example.car_management_app;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -13,6 +21,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -28,13 +37,36 @@ import kotlin.jvm.functions.Function2;
 public class Settings extends AppCompatActivity {
 
     private LinearLayout homeBack;
+    Switch switchAlarm;
+
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        String id = "my_channel_01";
+
         LinearLayout homeBack = (LinearLayout) findViewById(R.id.homeBack);
+        switchAlarm = findViewById(R.id.switchAlarm);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("myCh", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my Ch")
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setContentTitle("First Notification")
+                .setContentText("This is the Body of message")
+                .setOngoing(true);
+
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
 
         homeBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,4 +75,9 @@ public class Settings extends AppCompatActivity {
             }
         });
     }
+
+    public void push(View view){
+        notificationManagerCompat.notify(0, notification);
+    }
+
 }
